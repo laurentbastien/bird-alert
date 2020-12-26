374ac31cf704b2f2f9748a9714ac21eec6f12caf
laurent <22081051+laurentbastien@users.noreply.github.com>
2020-12-26 01:53:40 -0500

    Adding new bird monitor (#1)
    Co-authored-by: laurentbastien <laurent.corbeil@unity3d.com>

--- bird-alert/bird_alert/layout/alerts.py
+++ bird-alert/bird_alert/layout/alerts.py
@@ -1,32 +1,26 @@
 from rgbmatrix import graphics
 import time
-from bird_alert.bird_api.bird_client import BirdClient
-from bird_alert.constants import BirdContants
 
 class Alerts():
     def __init__(self, alert_font):
         self.alert_font = alert_font
 
-    @staticmethod
-    def __parse_text():
-        client = BirdClient(BirdContants.API_KEY.value)
-        return client.fetch_nearby_rare()
-
-    def set_alerts(self, matrix, pos):
+    def set_alerts(self, matrix, pos, spotted_bird):
         font = graphics.Font()
         font.LoadFont(self.alert_font)
-        textColor = graphics.Color(89, 158, 92)
-        alert_message = self.__parse_text()
+        textColor = graphics.Color(255, 255, 255)
 
-        graphics.DrawText(matrix, font, pos, 10, textColor, "my_text")
+
+        graphics.DrawText(matrix, font, 40, 10, graphics.Color(255, 255, 255), "ONE")
+        
+        #type of bird
+        graphics.DrawText(matrix, font, pos, 20, graphics.Color(255, 255, 255), spotted_bird.bird_name)
+
+        #location
+        total_length = graphics.DrawText(matrix, font, pos, 30, textColor, spotted_bird.bird_loc)
 
         pos -= 1
-        if (pos < 0):
-            pos = offscreen_canvas.width
 
         time.sleep(0.05)
         
-        matrix.SwapOnVSync(matrix)
-
-        return pos
-
+        return pos, total_length
