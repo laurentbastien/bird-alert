ba13dce9790afa477d8aaa038518a5bf5b69c00b
laurentbastien <laurent.corbeil@unity3d.com>
2020-12-28 15:47:09 -0500

    Fixed format

--- bird_alert/layout/bird_image.py
+++ bird_alert/layout/bird_image.py
@@ -1,13 +1,12 @@
 from PIL import Image
 
 
-class BirdImage():
+class BirdImage:
     def __init__(self, image_file):
         self.image_file = image_file
 
     def set_image(self, matrix):
         bird_image = Image.open(self.image_file)
-        bird_image.thumbnail(
-            (matrix.width-5, matrix.height-5), Image.ANTIALIAS)
+        bird_image.thumbnail((matrix.width - 5, matrix.height - 5), Image.ANTIALIAS)
 
-        matrix.SetImage(bird_image.convert('RGB'), 0, 15)
+        matrix.SetImage(bird_image.convert("RGB"), 0, 15)
