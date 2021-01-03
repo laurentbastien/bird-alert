05c9b796bedde4107458e744688c8839b94df1cc
laurent <22081051+laurentbastien@users.noreply.github.com>
2021-01-03 17:33:50 -0500

    More bird choices (#8)
    Co-authored-by: laurentbastien <laurent.corbeil@unity3d.com>

--- bird_alert/layout/bird_image.py
+++ bird_alert/layout/bird_image.py
@@ -2,11 +2,19 @@ from PIL import Image
 
 
 class BirdImage:
-    def __init__(self, image_file):
+    def __init__(self, image_file, bird_width, bird_height, loc_width, loc_height):
         self.image_file = image_file
+        self.bird_height = bird_height
+        self.bird_width = bird_width
+
+        self.loc_width = loc_width
+        self.loc_height = loc_height
 
     def set_image(self, matrix):
         bird_image = Image.open(self.image_file)
-        bird_image.thumbnail((matrix.width - 5, matrix.height - 5), Image.ANTIALIAS)
+        bird_image.thumbnail(
+            (matrix.width - self.bird_width, matrix.height - self.bird_width),
+            Image.ANTIALIAS,
+        )
 
-        matrix.SetImage(bird_image.convert("RGB"), 0, 15)
+        matrix.SetImage(bird_image.convert("RGB"), self.loc_width, self.loc_height)
